9c2eb662cd68eb3147bfbf5750ba94b9b4903b18
Nazish ahmed <arshiahmer@gmail.com>
2023-03-16 20:46:38 +0530

    expe

--- components/Experiencecardthree.tsx
+++ components/Experiencecardthree.tsx
@@ -14,17 +14,17 @@ function Experiencecardthree({}: Props) {
               transition={{ duration: .7 }}
               whileInView={{ opacity: 1, y: 0 }}
               viewport={{once: true}}
-              src="https://images-ext-2.discordapp.net/external/vjyVaxyWFiIH8OGFONRtRvvwvnLm2ihduLwQSCW7gVw/%3Fsize%3D4096/https/cdn.discordapp.com/avatars/950166445034188820/448f66351e7325c3f3d1cba0bd13a83f.webp?width=702&height=702"
+              src="https://images-ext-1.discordapp.net/external/tOoG1h4wr-o3-9ilqx23UrjzYxDztBY4aXrWIL3llDs/%3Fsize%3D4096/https/cdn.discordapp.com/avatars/880765863953858601/8375721897d9e015a3ca616c6ef52e9c.png?width=702&height=702"
           className='w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center' alt='NORI'
           />
           <div className='px-0 md:px-10'>
               <h4 className='text-4xl font-light'>Front-end developer</h4>
-              <p className='font-bold text-2xl mt-1'>SOFU</p>
+              <p className='font-bold text-2xl mt-1'>Gateway</p>
               <p className='uppercase py-5 text-gray-300'>Started work at 21-01-2023 - PART TIME</p>
               <ul className='list-disc space-y-4 ml-5 text-lg'>
                   <li>Worked as a web developer and a designer</li>
-                   <li>Developed the SOFU page using Typescript, next.js and Tailwind</li>
-                   <li>Designed the SOFU website</li>
+                   <li>Developed the Gateway page using Typescript, next.js and Tailwind</li>
+                   <li>Designed the Gateway website</li>
               </ul>
           </div>
     </article>
